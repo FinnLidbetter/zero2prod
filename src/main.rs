69b3a3d7ae587d93c52550314b615c454ed31d60
Finn Lidbetter <finnlidbetter@gmail.com>
2023-12-09 22:05:46 -0500

    Add CI Digital Ocean configuration

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 //! main.rs
 
-use secrecy::ExposeSecret;
 use std::net::TcpListener;
 
 use sqlx::PgPool;
@@ -15,8 +14,7 @@ async fn main() -> Result<(), std::io::Error> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool =
-        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
-            .expect("Failed to create Postgres connection pool.");
+        PgPool::connect_lazy_with(configuration.database.with_db());
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
