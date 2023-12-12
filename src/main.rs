51bd4dd5214e07c03ce7dc7c68c75d046d56f603
Finn Lidbetter <finnlidbetter@gmail.com>
2023-12-11 22:03:18 -0500

    Refactor test suite

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 //! main.rs
 
-use std::net::TcpListener;
-
-use sqlx::PgPool;
 use zero2prod::configuration::get_configuration;
-use zero2prod::email_client::EmailClient;
-use zero2prod::startup::run;
+use zero2prod::startup::Application;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
@@ -14,25 +10,8 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect_lazy_with(configuration.database.with_db());
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-    let email_base_url =
-        reqwest::Url::parse(&configuration.email_client.base_url).expect("Invalid base email url.");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        email_base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    )
-    .expect("Failed to construct email client.");
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool, email_client)?.await
+
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
+    Ok(())
 }
